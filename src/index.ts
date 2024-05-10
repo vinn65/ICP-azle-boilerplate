35455331e43914e085985bb523869e60d6fddde0
Sir Vin <109795015+vinn65@users.noreply.github.com>
2024-05-10 10:19:49 +0000

    starting

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-// cannister code goes here
+import { v4 as uuidv4 } from 'uuid';
+import { Server, StableBTreeMap, ic } from 'azle';
+import { Express } from 'express';
+
+class Message{
+    id:string;
+    title:string;
+    body:string;
+    attachmentUrl:string;
+    createdAt: Date;
+    updatedAt:string | null
+}
